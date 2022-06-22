57b135e5bae6cf0587f6add520aba3686812530c
nyaafree <45682975+nyaafree@users.noreply.github.com>
2022-06-23 02:26:45 +0900

    feat: stateの管理

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -5,6 +5,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2です</h1>
       <Link to="/page2/100">Url Parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,15 +1,29 @@
 // urlパラメータを扱いたい場合は下記の urlParamsをimportする
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   console.log(useParams());
   // オブジェクトの分割代入
   // urlParams() では {url:100} がreturn される
   const { id } = useParams();
+  const { search } = useLocation();
+  // useLocationの中身
+  // {
+  //   "pathname": "/page2/999",
+  //   "search": "?name=hogehoge",
+  //   "hash": ""
+  // }
+  console.log(search);
+
+  // URLSearchParamsはクエリパラメータを便利に扱うためのもの
+  const query = new URLSearchParams(search);
+
+  console.log(query);
   return (
     <div>
       <h1>UrlParameterです</h1>
       <p>パラメータは {id} です</p>
+      <p>クエリパラメータは {query.get("name")}です</p>
     </div>
   );
 };
